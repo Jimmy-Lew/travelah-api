965589d833a5d5b9baa23ab80e59c8b5d3306aa9
jimmy <jimmylew@gmail.com>
2022-06-22 11:17:40 +0800

    refactor: remove deprecated code

--- build/server.js
+++ build/server.js
@@ -35,5 +35,3 @@ router.use((req, res, next) => {
 const httpServer = http_1.default.createServer(router);
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 6060;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
-
-module.exports = router;
